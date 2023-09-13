f0ebad8afa8978a6ed62909978e653eda67f9163
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-09-13 15:27:52 -0300

    EventController update

--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.entities.Event;
 import com.devsuperior.demo.repositories.EventRepository;
 
@@ -21,6 +22,8 @@ public class EventService {
 		entity.setName(dto.getName());
 		entity.setDate(dto.getDate());
 		entity.setUrl(dto.getUrl());
+		entity.setCity(new City(dto.getCityId(),null));
+		
 		
 		entity = repository.save(entity);
 		return new EventDTO(entity);
